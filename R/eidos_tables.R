c06618e1dfc00443072db1743dd8b191d44f3204
hmirceb <hectorm94@gmail.com>
2025-08-03 19:30:47 +0200

    Add match.arg

--- R/eidos_tables.R
+++ R/eidos_tables.R
@@ -1,46 +1,36 @@
 #' Retrieve one of the tables related to the Spanish checklist
 #'
-#' @param table Name of the table to query. Must be one of "comunidades_autonomas", "listapatronespecie_codigos",
+#' @param table Name of the table to query. The function if case insensitive and admits whitespace, but the parameter must match one of "comunidades_autonomas", "listapatronespecie_codigos",
 #' "listapatronespecie", "componente_tema", "regbiogeograf_termar", "listapatronespecie_sinonimos", "pais",
-#' "norma", "provincias" or "listapatronespecie_normas"
+#' "norma", "provincias" or "listapatronespecie_normas".
 #'
 #' @returns The desired table from the EIDOS API
 #' @export
 #'
 #' @examples
 #' eidos_tables(eidos_table = "listapatronespecie")
-eidos_tables <- function(eidos_table = "listapatronespecie"){
-
-  # Clean names just in case:
+eidos_tables <- function(eidos_table = c("comunidades_autonomas",
+                                         "listapatronespecie_codigos",
+                                         "listapatronespecie",
+                                         "componente_tema",
+                                         "regbiogeograf_termar",
+                                         "listapatronespecie_sinonimos",
+                                         "pais",
+                                         "norma",
+                                         "provincias",
+                                         "listapatronespecie_normas")){
+  # Set table name in lower case and remove whitespaces just in case:
   eidos_table = gsub(pattern = " ",
                      replacement = "_",
                      x = tolower(
-                       x = eidos_table
+                       x = trimws(
+                         x = eidos_table
+                         )
                      )
   )
 
-  if(!eidos_table %in% c("comunidades_autonomas",
-                         "listapatronespecie_codigos",
-                         "listapatronespecie",
-                         "componente_tema",
-                         "regbiogeograf_termar",
-                         "listapatronespecie_sinonimos",
-                         "pais",
-                         "norma",
-                         "provincias",
-                         "listapatronespecie_normas")
-  ){
-    stop('The desired table is not available. Choose one of c("comunidades_autonomas",
-                         "listapatronespecie_codigos",
-                         "listapatronespecie",
-                         "componente_tema",
-                         "regbiogeograf_termar",
-                         "listapatronespecie_sinonimos",
-                         "pais",
-                         "norma",
-                         "provincias",
-                         "listapatronespecie_normas")')
-  }
+  # Match input to one of the arguments:
+  table = match.arg(eidos_table)
 
   # Set base URL
   base_url = "https://iepnb.gob.es/api/catalogo/"
@@ -48,7 +38,7 @@ eidos_tables <- function(eidos_table = "listapatronespecie"){
   # Create URL to desired table:
   table_url = paste0(base_url,
                      "v_", # for some reason all of them have this
-                     eidos_table)
+                     table)
 
   ## Query the API ##
   api_table = jsonlite::fromJSON(table_url)
