ced1f3840407dfb36db0246e4ac040a1a896cf0b
hmirceb <hectorm94@gmail.com>
2025-08-06 14:51:24 +0200

    Fixed clean names

--- R/eidos_clean_checklist.R
+++ R/eidos_clean_checklist.R
@@ -17,27 +17,30 @@ eidos_clean_checklist <- function(){
   # Remove unnecessary columns
   checklist = checklist[,-which(startsWith(x = names(checklist), prefix = "LP "))]
 
+  # Substitute "" for NA
+  checklist[checklist == ""] <- NA
+
   # Create new column with the taxon
-  checklist$taxon = ifelse(checklist$Sinónimo == "",
+  checklist$taxon = ifelse(is.na(checklist$Sinónimo),
                            checklist$ScientificName,
                            checklist$Sinónimo)
 
-  # Remove "subsp." and authorities
+  # Remove "subsp." and authorities and any Unicode whitespaces
   checklist$taxon_clean = sapply(checklist$taxon, clean_names)
 
   # With above information, generate full name excluding any possible leftovers
-  checklist$taxon_clean = ifelse(checklist$taxonRank == "Species",
-                                 sapply(strsplit(checklist$taxon_clean, split = " "), function(x){paste(x[1], x[2], sep = " ")}),
-                                 sapply(strsplit(checklist$taxon_clean, split = " "), function(x){paste(x[1], x[2], x[3], sep = " ")}))
+  checklist$taxon_clean = ifelse(checklist$taxonRank == "Subspecies" | grepl("subsp.", checklist$taxon),
+                                 sapply(strsplit(checklist$taxon_clean, split = " "), function(x){paste(x[1], x[2], x[3], sep = " ")}),
+                                 sapply(strsplit(checklist$taxon_clean, split = " "), function(x){paste(x[1], x[2], sep = " ")}))
 
-  # Remove any traces of NA and leading and trailing whitespaces
-  checklist$taxon_clean = trimws(
-    x = gsub(
-      pattern = "NA",
-      replacement = "",
-      x = checklist$taxon_clean
-      )
-    )
+  # Remove any leftover "NA"
+  checklist$taxon_clean = gsub(
+    pattern = "NA",
+    replacement = "",
+    x = checklist$taxon_clean)
+
+# Remove any traces of leading and trailing whitespaces
+  checklist$taxon_clean = trimws(checklist$taxon_clean)
 
   return(checklist)
 }
--- R/utils.R
+++ R/utils.R
@@ -11,12 +11,40 @@ clean_names = function(taxa_names){
   # Remove underscores if any
   taxa_names = gsub(pattern = "_", replacement = " ", x = taxa_names)
 
+  # Substitute Unicode whitespaces with normal whitespaces
+  taxa_names = gsub("\xc2\xa0", " ", taxa_names, fixed = TRUE)
+
+  # Remove anything between parentheses
+  taxa_names = gsub("\\(.*?\\)", "", taxa_names)
+
   # Split by whitespaces
   taxa_split = unlist(strsplit(taxa_names, split = " "))
 
-  # Detect "subsp.", "var." and non letter characters associated with
+  # Detect "subsp.", "var.", words starting in capital letter and
+  # non letter characters associated with
   # authorities except "-"
-  indices = which(grepl("^[[:upper:]]+|[^a-zA-Z-]", taxa_split)) # Check which words in the full name start with capital letter or have non-letter characters other than "-"
+  indice1 = which(grepl("^[[:upper:]]+|&|[^a-zA-Z-]]", taxa_split))
+
+  # Look for letters with umlaut "ö"
+  indice2 = which(grepl("^[[\U00E0-\U017F]]", taxa_split))
+
+  # Look for letters with dot
+  indice3 = which(grepl("\\.", taxa_split))
+
+  # Detect common words in names and authorities that
+  # might go undetected in previous checks
+  indice4 = which(taxa_split %in% c("ex", "et", "in",
+                                    "y", "zur", "sensu",
+                                    "pro", "parte", "de",
+                                    "la", "non", "da", "nud",
+                                    "del", "van", "der", "auct",
+                                    "den", "and", "-", "degli",
+                                    "en", "prensa", "subst",
+                                    "var", "species", "unrecognised", "x",
+                                    "subsp.", "var.", "subsp"))
+
+  # Join indices
+  indices = unique(c(indice1, indice2, indice3, indice4))
 
   # Collapse into full name:
   # The [-1] excludes the genus because they always start with capital letter
@@ -29,7 +57,7 @@ clean_names = function(taxa_names){
   }
 
   # Remove any extra whitespace just in case:
-  taxa_names_clean = gsub(pattern = "  ", replacement = "", x = taxa_names_clean)
+  taxa_names_clean = gsub(pattern = "\\s+", replacement = " ", x = taxa_names_clean)
   taxa_names_clean = trimws(taxa_names_clean)
 
   return(taxa_names_clean)
